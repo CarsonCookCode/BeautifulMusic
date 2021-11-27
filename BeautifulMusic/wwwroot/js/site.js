4973d988c83cf21035689c23666eca9e921b562a
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-11-27 12:32:14 -0700

    Add project files.

---
+++ BeautifulMusic/wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+let intViewportWidth = window.innerWidth;
+var id = null;
+function myMove() {
+    intViewportWidth = window.innerWidth;
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 1);
+    function frame() {
+        if (pos == (intViewportWidth - 50)) {
+            clearInterval(id);
+            myMoveBack();
+        } else {
+            pos++;
+            //elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+function myMoveBack() {
+    intViewportWidth = window.innerWidth;
+    var elem = document.getElementById("myAnimation");
+    var pos = (intViewportWidth - 50)
+    clearInterval(id);
+    id = setInterval(frame, 1);
+    function frame() {
+        if (pos == 0) {
+            clearInterval(id);
+            myMove();
+        } else {
+            pos--;
+            //elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+myMove();
